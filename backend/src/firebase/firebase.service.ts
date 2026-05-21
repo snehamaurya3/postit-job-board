5ad67c76b747f18ad0ae3abe9b9ad2858a1b05f3
snehamaurya3 <mauryasneha80@gmail.com>
2026-05-21 12:07:26 +0530

    Use firebase env variables

--- backend/src/firebase/firebase.service.ts
+++ backend/src/firebase/firebase.service.ts
@@ -1,19 +1,68 @@
-import { Injectable } from '@nestjs/common';
-import * as admin from 'firebase-admin';
-import * as serviceAccount from '../../firebase-service-account.json';
-
+import { Injectable } from '@nestjs/common'
+import * as admin from 'firebase-admin'
 
 @Injectable()
 export class FirebaseService {
-  public db: FirebaseFirestore.Firestore;
+
+  private firebaseApp: admin.app.App
 
   constructor() {
-    if (!admin.apps.length) {
-      admin.initializeApp({
-        credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
-      });
-    }
 
-    this.db = admin.firestore();
+    if (!admin.apps.length) {
+
+      this.firebaseApp =
+        admin.initializeApp({
+          credential:
+            admin.credential.cert({
+              projectId:
+                process.env
+                  .FIREBASE_PROJECT_ID,
+
+              clientEmail:
+                process.env
+                  .FIREBASE_CLIENT_EMAIL,
+
+              privateKey:
+                process.env
+                  .FIREBASE_PRIVATE_KEY
+                  ?.replace(/\\n/g, '\n'),
+            }),
+        })
+
+    } else {
+
+      this.firebaseApp =
+        admin.app()
+    }
+  }
+
+  getAuth() {
+    return admin.auth()
+  }
+
+  getFirestore() {
+    return admin.firestore()
   }
 }
+
+
+
+// import { Injectable } from '@nestjs/common';
+// import * as admin from 'firebase-admin';
+// import * as serviceAccount from '../../firebase-service-account.json';
+
+
+// @Injectable()
+// export class FirebaseService {
+//   public db: FirebaseFirestore.Firestore;
+
+//   constructor() {
+//     if (!admin.apps.length) {
+//       admin.initializeApp({
+//         credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
+//       });
+//     }
+
+//     this.db = admin.firestore();
+//   }
+// }
