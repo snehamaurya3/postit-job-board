7d1f66435a7a97143ddfb82709145d9816ae014d
snehamaurya3 <mauryasneha80@gmail.com>
2026-05-21 12:49:40 +0530

    Added vercel cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,13 @@ import { AppModule } from './app.module'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
 
-  app.enableCors()
+  // app.enableCors()
+  app.enableCors({
+  origin: [
+    'https://postit-job-board.vercel.app/',
+  ],
+  credentials: true,
+});
 
   await app.listen(3000)
 }
