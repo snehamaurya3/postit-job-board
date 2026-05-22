96536a84300be7618ac9ef562999988d7adbb312
snehamaurya3 <mauryasneha80@gmail.com>
2026-05-22 09:48:15 +0530

    updated cors origin

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,11 +5,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule)
 
   app.enableCors({
-    origin: [
-      'https://postit-job-board-qdxfu6su2-sneha-mauryas-projects-e94a42f9.vercel.app'
-    ],
-    credentials: true,
-  })
+  origin: true,
+  credentials: true,
+})
 
   await app.listen(3000)
 }
