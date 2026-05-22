02ed4984f34f903008b28d587709754b37696a50
snehamaurya3 <mauryasneha80@gmail.com>
2026-05-22 09:41:24 +0530

    fixed cors for deployed frontend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,19 +1,40 @@
 import { NestFactory } from '@nestjs/core'
-
 import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
 
-  // app.enableCors()
   app.enableCors({
-  origin: [
-    'https://postit-job-board.vercel.app/',
-  ],
-  credentials: true,
-});
+    origin: [
+      'https://postit-job-board-qdxfu6su2-sneha-mauryas-projects-e94a42f9.vercel.app'
+    ],
+    credentials: true,
+  })
 
   await app.listen(3000)
 }
 
 bootstrap()
+
+
+
+
+// import { NestFactory } from '@nestjs/core'
+
+// import { AppModule } from './app.module'
+
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule)
+
+//   // app.enableCors()
+//   app.enableCors({
+//   origin: [
+//     'https://postit-job-board.vercel.app/',
+//   ],
+//   credentials: true,
+// });
+
+//   await app.listen(3000)
+// }
+
+// bootstrap()
